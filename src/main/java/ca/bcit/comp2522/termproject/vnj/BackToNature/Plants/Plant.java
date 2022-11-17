bf9f90ea031fc488b3a75a39b52cf40ced8b14ce
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2022-11-17 13:17:15 -0800

    Modified Plant classes

--- src/main/java/ca/bcit/comp2522/termproject/vnj/BackToNature/Plants/Plant.java
+++ src/main/java/ca/bcit/comp2522/termproject/vnj/BackToNature/Plants/Plant.java
@@ -1,5 +1,7 @@
 package ca.bcit.comp2522.termproject.vnj.BackToNature.Plants;
 
+import java.util.Objects;
+
 /**
  * Crop Class. When growing a plant, in this version, there are three stages
  * seed, budding and ripening. By default, the plant starts as a seed.
@@ -22,6 +24,11 @@ public abstract class Plant {
      * The number of days the crops has been planted.
      */
     protected int days;
+
+    /**
+     * The current growth stage of the plant.
+     */
+    protected Growth stage;
     /**
      * Constructs an object of type crop.
      *
@@ -29,6 +36,7 @@ public abstract class Plant {
      */
     public Plant() {
         this.days = START_PLANTED;
+        this.stage = Growth.seed;
     }
 
     /**
@@ -41,33 +49,20 @@ public abstract class Plant {
     }
 
     /**
+     * Returns the number of days the plant has been planted.
+     *
+     * @return days as an int
+     */
+    public Growth getStage() {
+        return stage;
+    }
+
+    /**
      * Increments day and grows the plant to a new stage.
      */
     public void grow() {
         this.days++;
     }
-
-    /**
-     * Returns the current stage of the crop.
-     *
-     * @return stage of the crop as a String.
-     */
-    public Growth stage() {
-        return Growth.budding;
-    }
-
-    /**
-     * Turns the plant into budding stage.
-     */
-    protected void budding() {
-    }
-
-    /**
-     * Turns the crop into ripening stage.
-     */
-    protected void ripening() {
-    }
-
     /**
      * Returns a description of a Crop object.
      *
@@ -78,5 +73,33 @@ public abstract class Plant {
         return "Crop{"
                 + "days=" + days
                 + '}';
+    }
+
+    /**
+     * Checks if an object is identical to the instance.
+     *
+     * @param object an object
+     * @return true if an object is the same as the instance
+     */
+    @Override
+    public boolean equals(final Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Plant plant = (Plant) object;
+        return days == plant.days && stage == plant.stage;
+    }
+
+    /**
+     * Creates a hashcode associated with the instance.
+     *
+     * @return an int
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(days, stage);
     }
 }
--- src/main/java/ca/bcit/comp2522/termproject/vnj/BackToNature/Plants/Turnip.java
+++ src/main/java/ca/bcit/comp2522/termproject/vnj/BackToNature/Plants/Turnip.java
@@ -15,32 +15,48 @@ public class Turnip extends Plant implements Harvestable {
      * Default name of a turnip crop.
      */
     public static final String DEFAULT_CROP_NAME = "Turnip";
-
+    /**
+     * Default number of days for the seed to go budding stage.
+     */
+    public static final int DEFAULT_NUM_TO_BUDDING = 2;
+    /**
+     * Default number of days for the bud to go ripening stage.
+     */
+    public static final int DEFAULT_NUM_TO_RIPENING = 4;
+    /**
+     * Grows the plant into different stages.
+     */
     @Override
-    public void budding() {
-    }
-
-    @Override
-    public void ripening() {
+    public void grow() {
+        this.days++;
+        if (this.days >= DEFAULT_NUM_TO_BUDDING && this.days <= DEFAULT_NUM_TO_RIPENING) {
+            this.stage = Growth.budding;
+        }
+        if (this.days >= DEFAULT_NUM_TO_RIPENING) {
+            this.stage = Growth.ripe;
+        }
     }
 
     /**
-     * Creates an instance.
+     * Creates a sellable crop if the plant is in ripening.
      *
-     * @return a Turnip crop
+     * @return a turnip crop object if the plant is in ripening stage else null
      */
     @Override
     public Crop harvest() {
-        class TurnipCrop extends Crop {
-            /**
-             * A Crop constructor.
-             *
-             * @throws IllegalArgumentException when price is below 0
-             */
-            TurnipCrop() {
-                super(DEFAULT_PRICE, DEFAULT_CROP_NAME);
+        if (this.stage == Growth.ripe) {
+            class TurnipCrop extends Crop {
+                /**
+                 * A Crop constructor.
+                 *
+                 * @throws IllegalArgumentException when price is below 0
+                 */
+                TurnipCrop() {
+                    super(DEFAULT_PRICE, DEFAULT_CROP_NAME);
+                }
             }
+            return new TurnipCrop();
         }
-        return new TurnipCrop();
+        return null;
     }
 }
